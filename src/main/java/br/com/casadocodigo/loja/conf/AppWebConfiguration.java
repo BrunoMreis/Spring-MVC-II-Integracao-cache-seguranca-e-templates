ff6ae33b0961e38bc5d8827df8b8b7231f30a0fb
BrunoMreis <brbrno@hotmail.com>
2026-07-09 21:55:24 -0300

    Removendo imports sem uso.

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,33 +1,25 @@
 package br.com.casadocodigo.loja.conf;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
 import org.springframework.lang.NonNull;
-import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 
@@ -35,9 +27,6 @@ import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Configuration
 @ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
@@ -78,16 +67,4 @@ public class AppWebConfiguration implements WebMvcConfigurer {
 				.expireAfterAccess(5, TimeUnit.MINUTES));
 		return cacheManager;
 	}
-
-	// @Bean
-	// public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager,
-	// 		ThymeleafViewResolver thymeleafViewResolver) {
-	// 	List<ViewResolver> resolvers = new ArrayList<>();
-	// 	resolvers.add(thymeleafViewResolver);
-	// 	resolvers.add(new JsonViewResolver());
-	// 	ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-	// 	resolver.setViewResolvers(resolvers);
-	// 	resolver.setContentNegotiationManager(manager);
-	// 	return resolver;
-	// }
 }
